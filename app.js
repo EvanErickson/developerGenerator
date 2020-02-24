fd156fbd30b47930679860a76fb7828e67a3b8b5
EvanErickson <evanserickson@gmail.com>
2020-02-24 15:01:03 -0800

    work

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const prompt = require('inquirer').createPromptModule()
+const fs = require('fs')
+
+prompt([
+    {
+        type: "input",
+        name: 'githubUsername',
+        message: 'What is your Github Username?'
+    }
+]) .then((response) => {
+    console.log(response);
+})
+
+
