4e8f0fcfb844ba0f1058b818e3cbfa221acd6b17
EvanErickson <evanserickson@gmail.com>
2020-02-26 12:28:31 -0800

    everything working except 'ask any question to david' part at the end

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
-# developerGenerator
 
-# 1. Please install the node modules (npm i)
+# ReadmeGenerator
 
-# 2. Answer all prompts and FS will create the file
+## Description
+Create a readMe document for any project.
+
+## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Test](#test)
+  * [Questions](#questions)
+  
+## Installation
+undefined
+
+## Usage
+undefined
+
+## License
+Open Source. Please give credit to EvanErickson on Github if you want to use it. Please also fork and star my repo.
+
+## Contributing
+Feel free to contribute. As of this moment, the app was built by Evan Erickson and Aria Sadeghi.
+
+## Test
+N/A
+
+## Questions
+![User Img](https://avatars3.githubusercontent.com/u/11791361?v=4)
+
+Please ask any questions to David at null.
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const userInfo = require('./utils/api.js');
 const prompt = require('inquirer').createPromptModule()
-const fs = require('fs')
 const axios = require('axios')
+const path = require('path');
+const api = require('./utils/api.js');
+const fs = require('fs')
 
 // Questions array
 const questions = [
@@ -26,10 +30,48 @@ function init() {
             name: 'username',
             message: questions[0]
         },
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[1]
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: questions[2]
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: questions[3]
+        },
+        {
+            type: 'input',
+            name: 'use',
+            message: questions[4]
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: questions[5]
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: questions[6]
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: questions[7]
+        }   
     ]) .then(response => {
-        fs.writeFile('dev.md', `Github Username: ${response.githubUsername}`, function (err) {
-            if (err) throw err;
-            console.log('File created successfully.');
+        
+        api
+        .getUser(response.user)
+        .then(({ data }) => {
+        
+          writeToFile("README.md", generateMarkdown({ ...data, ...response }));
         })
     })
 }
