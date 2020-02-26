b140f95891f6318ce174b6127a105bfbd5154dac
EvanErickson <evanserickson@gmail.com>
2020-02-26 12:18:30 -0800

    almost done

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
-const questions = [
+const prompt = require('inquirer').createPromptModule()
+const fs = require('fs')
+const axios = require('axios')
 
+// Questions array
+const questions = [
+    "What is your GitHub username?",
+    "What is the title of your project?",
+    'Please enter a project description:',
+    "Please enter an installation guide:",
+    "How does the user use this app?",
+    "What does the user need to know about the license?",
+    "What does the user need to know about contributions?",
+    "What command would you run to test this app?"
 ];
 
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
+// Prompt with all the questions
 function init() {
-
+    prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: questions[0]
+        },
+    ]) .then(response => {
+        fs.writeFile('dev.md', `Github Username: ${response.githubUsername}`, function (err) {
+            if (err) throw err;
+            console.log('File created successfully.');
+        })
+    })
 }
 
 init();
 
 
-
-const prompt = require('inquirer').createPromptModule()
-const fs = require('fs')
-const axios = require('axios')
-
-prompt([
-    {
-        type: "input",
-        name: 'githubUsername',
-        message: 'What is your Github Username?'
-    }
-]) .then((response) => {
-    fs.writeFile('dev.md', `Github Username: ${response.githubUsername}`, function (err) {
-        if (err) throw err;
-        console.log('File created successfully.');
-    })
-})
--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,13 @@
+const axios = require('axios');
+
+
 const api = {
   getUser(username) {
+    return axios.get(`https://api.github.com/users/${username}`)
+    .catch(function (error) {
 
+      console.log(error);
+    });
   }
 };
 
